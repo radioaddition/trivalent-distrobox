274ae4bf876f8babd606e3f5fa1866e6ffb16eab
RadioAddition <radioaddition@pm.me>
2025-01-20 13:18:28 -0500

    change base image to work with distrobox

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#./distrobox-shims.sh
+curl https://copr.fedorainfracloud.org/coprs/secureblue/hardened_malloc/repo/fedora-41/secureblue-hardened_malloc-fedora-41.repo -o /etc/yum.repos.d/secureblue-hardened_malloc-fedora-41.repo
 curl https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-41/secureblue-trivalent-fedora-41.repo -o /etc/yum.repos.d/secureblue-trivalent-fedora-41.repo
-rpm-ostree refresh-md
-rpm-ostree update
-grep -v '^#' ./trivalent.packages | xargs rpm-ostree install -y
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf refresh-md
+dnf update
+grep -v '^#' ./trivalent.packages | xargs dnf install -y
