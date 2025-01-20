4ad2da18a15a7d3e5331aae8605bfdf4fbc78c44
RadioAddition <radioaddition@pm.me>
2025-01-20 03:21:46 -0500

    testing something

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -3,7 +3,8 @@
 # Symlink distrobox shims
 ./distrobox-shims.sh
 
-dnf5 copr enable secureblue/trivalent
+#./copr.sh enable secureblue/trivalent
 # Update the container and install packages
-rpm-ostree update -y
-grep -v '^#' ./trivalent.packages | xargs rpm-ostree install
+rpm-ostree update
+rpm-ostree --enablerepo secureblue/trivalent
+grep -v '^#' ./trivalent.packages | xargs rpm-ostree install -y
