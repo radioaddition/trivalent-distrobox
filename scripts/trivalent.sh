bf9e8e7404b5c9b14c5e9df83543c1c1caaf87e3
RadioAddition <radioaddition@pm.me>
2025-01-20 12:23:14 -0500

    this should work fs

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Symlink distrobox shims
 ./distrobox-shims.sh
-
-ostree remote add trivalent https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-41/secureblue-trivalent-fedora-41.repo
-rpm-ostree --enablerepo=trivalent refresh-md
+wget https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-41/secureblue-trivalent-fedora-41.repo -o /etc/yum.repos.d/
+rpm-ostree refresh-md
 rpm-ostree update
 grep -v '^#' ./trivalent.packages | xargs rpm-ostree install -y
