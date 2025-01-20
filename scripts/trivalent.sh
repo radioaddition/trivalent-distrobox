78234b994eb346c1e37063f95ba74038d93e3e2a
RadioAddition <radioaddition@pm.me>
2025-01-20 12:34:43 -0500

    this one works definitely

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./distrobox-shims.sh
-wget https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-41/secureblue-trivalent-fedora-41.repo -o /etc/yum.repos.d/
+#./distrobox-shims.sh
+curl https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-41/secureblue-trivalent-fedora-41.repo -o /etc/yum.repos.d/secureblue-trivalent-fedora-41.repo
 rpm-ostree refresh-md
 rpm-ostree update
 grep -v '^#' ./trivalent.packages | xargs rpm-ostree install -y
