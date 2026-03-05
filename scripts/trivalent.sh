9423bc69b94e61da627535812e28f68522905a94
RadioAddition <radioaddition@pm.me>
2026-03-04 20:11:29 -0500

    fix wrong repo url

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl https://copr.fedorainfracloud.org/coprs/secureblue/hardened_malloc/repo/fedora-$(rpm -E %fedora)/secureblue-hardened_malloc-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-hardened_malloc-fedora-$(rpm -E %fedora).repo
-curl https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-$(rpm -E %fedora)/secureblue-trivalent-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-trivalent-fedora-$(rpm -E %fedora).repo
+curl https://copr.fedorainfracloud.org/coprs/secureblue/packages/repo/fedora-$(rpm -E %fedora)/secureblue-packages-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-packages-fedora-$(rpm -E %fedora).repo
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf makecache --refresh
 dnf update
