10f438e7cbda3ed54e146603ca385548a50e1588
RadioAddition <radioaddition@pm.me>
2026-03-04 20:01:02 -0500

    fix attempt

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl https://copr.fedorainfracloud.org/coprs/secureblue/hardened_malloc/repo/fedora-41/secureblue-hardened_malloc-fedora-41.repo -o /etc/yum.repos.d/secureblue-hardened_malloc-fedora-41.repo
-curl https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-41/secureblue-trivalent-fedora-41.repo -o /etc/yum.repos.d/secureblue-trivalent-fedora-41.repo
+curl https://copr.fedorainfracloud.org/coprs/secureblue/hardened_malloc/repo/fedora-$(rpm -E %fedora)/secureblue-hardened_malloc-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-hardened_malloc-fedora-$(rpm -E %fedora).repo
+curl https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-$(rpm -E %fedora)/secureblue-trivalent-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-trivalent-fedora-$(rpm -E %fedora).repo
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf refresh-md
 dnf update
