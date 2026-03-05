54875ebfef5104b087960b61d8a5256251ddcb3e
RadioAddition <radioaddition@pm.me>
2026-03-04 20:05:08 -0500

    fix use of deprecated subcommand

--- scripts/trivalent.sh
+++ scripts/trivalent.sh
@@ -3,6 +3,6 @@
 curl https://copr.fedorainfracloud.org/coprs/secureblue/hardened_malloc/repo/fedora-$(rpm -E %fedora)/secureblue-hardened_malloc-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-hardened_malloc-fedora-$(rpm -E %fedora).repo
 curl https://copr.fedorainfracloud.org/coprs/secureblue/trivalent/repo/fedora-$(rpm -E %fedora)/secureblue-trivalent-fedora-$(rpm -E %fedora).repo -o /etc/yum.repos.d/secureblue-trivalent-fedora-$(rpm -E %fedora).repo
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf refresh-md
+dnf makecache --refresh
 dnf update
 grep -v '^#' ./trivalent.packages | xargs dnf install -y
